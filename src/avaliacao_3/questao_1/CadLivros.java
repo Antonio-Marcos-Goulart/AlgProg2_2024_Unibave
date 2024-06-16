381a00664c3dcc6a474b81d20c455f9892a4cf92
Antônio Marcos <antoniomarcos5674335@gmail.com>
2024-06-16 00:20:13 -0300

    Prova 3

--- src/avaliacao_3/questao_1/CadLivros.java
+++ src/avaliacao_3/questao_1/CadLivros.java
@@ -1,24 +1,19 @@
 package avaliacao_3.questao_1;
 
-
 import javax.swing.*;
 import java.util.ArrayList;
 
 public class CadLivros {
     public static void main(String[] args) {
-
-        int op = 0;
         ArrayList<Livros> livros = new ArrayList<Livros>();
 
-        do {
-            op = menu();
-            if (op == 1) livros.add(cadastroLivro());
-            if (op == 2) localizarLivroISBN_mostrarPessoas(livros);
-            if (op == 3) usuario_livros(livros);
-            if (op == 4) livrosPublicadosEm2020(livros);
-            if (op == 5) livrosPorGenero(livros);
-        } while (op != 6);
+        int op = 0;
 
+        do{
+            op = menu();
+            if(op==1) livros.add(cadLivro());
+
+        }while (op!=6);
     }
 
     private static int menu() {
@@ -28,149 +23,63 @@ public class CadLivros {
                 + "4 - Dados de todos os livros publicados em 2020\n"
                 + "5 - Lista de livros de um determinado gênero \n\n"
                 + "6 - SAIR";
-        return Integer.parseInt(JOptionPane.showInputDialog(m));
+        return Integer.parseInt(JOptionPane.showInputDialog(null, m));
     }
 
-    private static Livros cadastroLivro() {
-        Livros livro = new Livros();
-
-// TITULO DO LIVRO
-
-        String titulo;
-        do {
-            titulo = JOptionPane.showInputDialog("Digite o titulo do livro:");
-            if (titulo == null || titulo.trim().isEmpty()) {
-                JOptionPane.showInputDialog("Digite o titulo do livro:");
-            }
-        } while (titulo == null || titulo.trim().isEmpty());
-        livro.setTitulo(titulo);
-
-// AUTOR
-
-        String autor;
-        do {
-            autor = JOptionPane.showInputDialog("Digite o nome do autor:");
-            if (autor == null || autor.trim().isEmpty()) {
-                JOptionPane.showInputDialog("Digite o nome do autor:");
-            }
-        } while (autor == null || titulo.trim().isEmpty());
-        livro.setAutor(autor);
-
-// ANO DE PUBLICAÇÃO
-
-        int anoDePublicacao = 0;
-        boolean anoPublicacaoValido = false;
-        do {
-            try {
-                String ano = JOptionPane.showInputDialog("Digite o ano de publicação do livro " + livro.getTitulo());
-                if (ano == null) {
-                    JOptionPane.showInputDialog("Digite o ano de publicação do livro " + livro.getTitulo());
-                }
-                anoDePublicacao = Integer.parseInt(ano);
-                anoPublicacaoValido = true;
-            } catch (NumberFormatException e) {
-                JOptionPane.showInputDialog(null, "Insira um ano valido!");
-            }
-        } while (!anoPublicacaoValido);
-        livro.setAnoDePublicacao(anoDePublicacao);
-
-// GENERO
-
-        String genero;
-        do {
-            genero = JOptionPane.showInputDialog("Digite o genêro do livro:" + livro.getTitulo());
-            if (genero == null || genero.trim().isEmpty()) {
-                JOptionPane.showInputDialog("Digite o genêro do livro:" + livro.getTitulo());
-            }
-        } while (genero == null || titulo.trim().isEmpty());
-        livro.setGenero(genero);
-
-// ISBN
-
-        long isbn = 0; // LONG POIS INT NÃO FUNCIONOU CORRETAMENTE PELA QUANTIDADE DE NUMEROS
-        boolean isbnNum = false;
-        do {
-            try {
-                String numIsbn = JOptionPane.showInputDialog("Digite o ISBN do livro\nSérie de 13 números inteiros: " + livro.getNumeroDeLivroPadraoInternacional_ISBN());
-                if (numIsbn == null) {
-                    JOptionPane.showInputDialog("Digite o ISBN do livro\nSérie de 13 números inteiros: " + livro.getNumeroDeLivroPadraoInternacional_ISBN());
-                }
-                if (numIsbn.length() != 13) {
-                    throw new NumberFormatException(); // ISBN deve ter 13 dígitos
-                }
-
-                isbn = Long.parseLong(numIsbn); // usa long para ISBN
-                isbnNum = true;
-            } catch (NumberFormatException e) {
-                JOptionPane.showMessageDialog(null, "Insira um ISBN válido de 13 dígitos!");
-            }
-        } while (!isbnNum);
-
-        livro.setNumeroDeLivroPadraoInternacional_ISBN(isbn);
-
-// PESSOAS QUE PEGARAM O LIVRO
-
-        int n = Integer.parseInt(JOptionPane.showInputDialog("Quantidade de pessoas que pegaram o livro emprestado: "));
-        String[] pessoasQuePegaramLivroEmprestado = new String[n];
-
-        for (int i = 0; i < n; i++) {
-            pessoasQuePegaramLivroEmprestado[i] = JOptionPane.showInputDialog("Digite o nome da pessoa n° " + (i + 1) + ": ");
-
+    //exibir mensagem
+    public static void msg(String txt) {
+        JOptionPane.showMessageDialog(null, txt);
+    }
+    //cadastro de dados do livro
+    public static String lerString(String txt) {
+        String recString = JOptionPane.showInputDialog(txt);
+        if(recString.trim().isEmpty()||recString.length()<=3) {
+            msg("Dados informados inválidos, verifique os dados e tente novamente!");
+            return lerString(txt);
+        } else {
+            return recString;
         }
-        livro.setPessoasQuePegaramLivroEmprestado(pessoasQuePegaramLivroEmprestado);
-        return livro;
     }
 
-//================================================
-
-    private static void localizarLivroISBN_mostrarPessoas(ArrayList<Livros> livros) { // mostra as pessoas que ja pegaram os livros - usar um array
-        if (livros.isEmpty()) {
-            JOptionPane.showMessageDialog(null, "Nenhum livro cadastrado!");
-            return;
+    public static int lerInt(String txt) {
+        int recInt = Integer.parseInt(JOptionPane.showInputDialog(txt));
+        if(recInt <= 0 ) {
+            msg("Dados informados inválidos, verifique os dados e tente novamente!");
+            return lerInt(txt);
+        }else {
+            return recInt;
         }
-
     }
 
-    private static void usuario_livros(ArrayList<Livros> livros) { // mostra os livros que fulano pegou
-        if (livros.isEmpty()) {
-            JOptionPane.showMessageDialog(null, "Nenhum livro cadastrado!");
-            return;
+    public static long lerLong(String txt) {
+        long recLong = Long.parseLong(JOptionPane.showInputDialog(txt));
+        if (Long.toString(recLong).length() != 13) {
+            msg("Dados informados inválidos, verifique os dados e tente novamente!");
+            return lerLong(txt);
+        } else {
+            return recLong;
         }
-
-        String nomeUsuarioLivro = JOptionPane.showInputDialog("Digite o nome do usuário:");
-
-        if (nomeUsuarioLivro == null || nomeUsuarioLivro.trim().isEmpty()){
-
-        }
-
     }
 
-    private static void livrosPublicadosEm2020(ArrayList<Livros> livros) { // livros publicados no ano de 2020
-        if (livros.isEmpty()){
-            JOptionPane.showMessageDialog(null, "Nenhum livro cadastrado!");
-            return;
-        }
-        ArrayList<Livros>livro2020 = new ArrayList<>();
-        for (Livros livro : livros){
-            if (livro.getAnoDePublicacao() == 2020){
-                livro2020.add(livro);
+// CADASTRO DO LIVRO
+    public static Livros cadLivro() {
+        Livros lv = new Livros();
+        int emp = 0;
+        lv.titulos = lerString("Título:");
+        lv.autores = lerString("Autores:");
+        lv.anoDePublicacao = lerInt("Ano de publicação");
+        lv.generos = lerString("Gênero:");
+        lv.ISBNs = lerLong("ISBN:");
+
+        do {
+            emp = Integer.parseInt(JOptionPane.showInputDialog("""
+                    1 - Cadastrar novo empréstimo
+                    2 - Sair
+                    """));
+            if (emp == 1) {
+                lv.nomeEmprestimos.add(lerString("Nome de quem pegou emprestado:"));
             }
-        }
-        if (livro2020.isEmpty()) {
-            JOptionPane.showMessageDialog(null, "Nenhum livro do ano de 2020 cadastrado!");
-            return;
-        }
-
-        StringBuilder livro2020msg = new StringBuilder("Livros publicados em 2020:\n");
-        for (Livros livro : livro2020){
-            livro2020msg.append(livro.toString2020()).append("\n");
-        }
-        JOptionPane.showMessageDialog(null, livro2020msg.toString());
-    }
-
-    private static void livrosPorGenero(ArrayList<Livros> livros) { // filtra os livros por generos
-
-
-
+        } while (emp != 2);
+        return lv;
     }
 }
--- src/avaliacao_3/questao_1/Livros.java
+++ src/avaliacao_3/questao_1/Livros.java
@@ -1,66 +1,12 @@
 package avaliacao_3.questao_1;
 
+import java.util.ArrayList;
+
 public class Livros {
-    String titulo;
-    String genero;
-    String autor;
+    String titulos;
+    String generos;
+    String autores;
     int anoDePublicacao;
-    long numeroDeLivroPadraoInternacional_ISBN; // USANDO LONG POIS COM INT NÃO FUNIONA CORRETAMENTE
-    String[] pessoasQuePegaramLivroEmprestado;
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String tiluo) {
-        this.titulo = tiluo;
-    }
-
-    public String getGenero() {
-        return genero;
-    }
-
-    public void setGenero(String genero) {
-        this.genero = genero;
-    }
-
-    public String getAutor() {
-        return autor;
-    }
-
-    public void setAutor(String autor) {
-        this.autor = autor;
-    }
-
-    public Integer getAnoDePublicacao() {
-        return anoDePublicacao;
-    }
-
-    public void setAnoDePublicacao(Integer anoDePublicacao) {
-        this.anoDePublicacao = anoDePublicacao;
-    }
-
-    public long getNumeroDeLivroPadraoInternacional_ISBN() {
-        return numeroDeLivroPadraoInternacional_ISBN;
-    }
-
-    public void setNumeroDeLivroPadraoInternacional_ISBN(long numeroDeLivroPadraoInternacional_ISBN) {
-        this.numeroDeLivroPadraoInternacional_ISBN = numeroDeLivroPadraoInternacional_ISBN;
-    }
-
-    public String[] getPessoasQuePegaramLivroEmprestado() {
-        return pessoasQuePegaramLivroEmprestado;
-    }
-
-    public void setPessoasQuePegaramLivroEmprestado(String[] pessoasQuePegaramLivroEmprestado) {
-        this.pessoasQuePegaramLivroEmprestado = pessoasQuePegaramLivroEmprestado;
-    }
-
-
-    public String toString2020() {
-        return "Titulo do Livro: " + titulo + "\n" +
-                "Autor: " + autor + "\n";
-    }
-
-
+    long ISBNs;
+    ArrayList<String> nomeEmprestimos = new ArrayList<>();
 }
